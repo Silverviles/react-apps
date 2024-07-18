8699d97076c2b8e5f303158e168deb616e5540a7
Tharindu Siriwardhana <tharindu.siriwardhana@avtra.com>
2024-07-18 03:09:04 +0000

    login-signup form basic layout

---
+++ login-signup/src/Components/LoginSignup/LoginSignup.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./LoginSignup.css";
+
+import user_icon from '../Assets/person.png';
+import email_icon from '../Assets/email.png';
+import password_icon from '../Assets/password.png';
+
+export const LoginSignup = () => {
+	return (
+		<div className={'container'}>
+			<div className={'header'} >
+				<div className={'text'}>Sign Up</div>
+				<div className={'underline'}></div>
+			</div>
+			<div className={'inputs'}>
+				<div className={'input'}>
+					<img src={user_icon} alt={'user icon'}/>
+					<input type={'text'} placeholder={'username'}/>
+				</div>
+				<div className={'input'}>
+					<img src={email_icon} alt={'email icon'}/>
+					<input type={'email'} placeholder={'email'}/>
+				</div>
+				<div className={'input'}>
+					<img src={password_icon} alt={'password icon'}/>
+					<input type={'password'} placeholder={'password'}/>
+				</div>
+			</div>
+			<div className={'forgot-password'}>Lost Password? <span>Click Here!</span></div>
+			<div className={'submit-container'}>
+				<button className={'submit'}>Sign Up</button>
+				<button className={'submit'}>Log In</button>
+			</div>
+		</div>
+	);
+}
